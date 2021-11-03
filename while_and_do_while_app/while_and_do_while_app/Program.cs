91923bf37584e35c5374e61e2f51de93f96f87d1
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-11-02 17:49:25 -0700

    Add files via upload

---
+++ while_and_do_while_app/while_and_do_while_app/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace while_and_do_while_app
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess which nfl team is the best!");
+            Console.ReadLine();
+            string team = "cowboys";
+            string guess = "";
+            while (guess != team)
+            {
+                Console.WriteLine("Guess again!");
+                guess = Console.ReadLine();
+                
+            }
+            Console.WriteLine("That is correct!");
+            {
+                int num = 100;
+
+                do
+                {
+                    Console.WriteLine("value of number: {0}", num);
+                    num = num + 1;
+                }
+                while (num < 20);
+                Console.ReadLine();
+            }
+        }
+    }
+}
