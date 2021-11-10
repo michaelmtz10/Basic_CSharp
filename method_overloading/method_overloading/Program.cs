ba483bafc50e9af8e754735462337939818f4287
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-11-09 22:06:42 -0800

    Add files via upload

---
+++ method_overloading/method_overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method overload 1
+            Console.WriteLine("Type in a number that will be added");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            overload overload_1 = new overload(); // instance of method is created
+            int total = overload_1.the_method(input); // total is the sum of number input plugged into method wih
+            Console.WriteLine(total);                  // pre created math operation
+            
+            // method overload 2
+            Console.WriteLine("Type in a decimal number..");
+            decimal desi = Convert.ToDecimal(Console.ReadLine()); // converts input to decimal
+
+            overload overload_2 = new overload();
+            decimal amount = overload_2.the_method(desi); // amount is created when input given and method is applied
+            Console.WriteLine(amount);
+
+            // method overload 3
+            Console.WriteLine("Type in the actual word of a valid number..");
+            string num = Convert.ToString(Console.ReadLine()); // converts user input to string
+            overload overload_3 = new overload(); // new instance of method
+            string output = overload_3.the_method(num); // output will equal nethod with method string to int
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ method_overloading/method_overloading/overload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_overloading
+{
+    class overload
+    {
+        internal int the_method(int a)
+        {
+            int total = a + 100;
+
+            return total;
+        }
+        internal decimal the_method(decimal b)
+        {
+            decimal amount = b * 100;
+
+            return amount;
+        }
+        internal string the_method(string num)
+        {
+            int num_converted;
+            num_converted = Int32.Parse(num);
+
+            return num;
+        }
+    }
+}
