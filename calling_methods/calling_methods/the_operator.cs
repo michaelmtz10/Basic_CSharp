f4a33b582b89cb534426ee7e5c60f8a3ce0cc70b
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-11-09 22:10:59 -0800

    Add files via upload

---
+++ calling_methods/calling_methods/the_operator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calling_methods
+{
+    class the_operator{
+        internal int multiplier (int a)
+        {
+            int total = a * 10;
+
+            return total;
+
+        }
+        internal int diviser (int x)
+        {
+            int answer = x / 10;
+
+            return answer;
+        }
+        internal int adder (int f)
+        {
+            int feedback = f + 10;
+
+            return feedback;
+        }
+
+    }
+}
