2c5d4fced3940ee156293ea3f4f62f7c12eea2a7
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-12 00:13:41 -0800

    Add project files.

---
+++ method_assignment/Choices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace method_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number of choice..");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in another number if desired.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Options(a, b);
+            Console.Read();
+         
+        }
+
+        private static void Options(int a, int b = 80)
+        {
+             Console.WriteLine("Answer :" + a * b);
+        }
+    }
+}
