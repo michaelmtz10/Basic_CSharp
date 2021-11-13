d355b56db0caf2b2dea9a68daf310b0d5caa2d25
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-13 00:35:08 -0800

    method assignment

--- method_assignment/Choices.cs
+++ method_assignment/Choices.cs
@@ -6,18 +6,23 @@ namespace method_assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type in a number of choice..");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Type in another number if desired.");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Options(a, b);
+        try
+            {
+                Console.WriteLine("Type in a number of choice..");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Type in another number if desired.");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                optional result = new optional();
+                int output = result.Options(a, b);
+                Console.WriteLine(output);
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Input is not valid.");
+            }
             Console.Read();
          
-        }
-
-        private static void Options(int a, int b = 80)
-        {
-             Console.WriteLine("Answer :" + a * b);
         }
     }
 }
---
+++ method_assignment/optional.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_assignment
+{
+    class optional
+    {
+        internal int Options(int a, int b = 80)
+        {
+            return a * b;
+        }
+    }
+}
