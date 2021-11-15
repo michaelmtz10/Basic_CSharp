119c5491f44038490dc3a5fe55ea9c53f3087169
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-14 18:31:50 -0800

    void_method
    void method

---
+++ Void_method/Void_method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Void_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int response_1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter a second number..");
+                int response_2 = Convert.ToInt32(Console.ReadLine());
+
+                void_method my_instance = new void_method();
+                Console.WriteLine(my_instance.Solver(response_1, response_2));
+            }
+        }
+    }
+}
---
+++ Void_method/Void_method/void_method.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_method
+{
+    public class void_method
+    {
+        public int response_1 { get; set; }
+        public int response_2 { get; set; }
+        public void Solver(int x, int y)
+        {
+            response_1 = x;
+            response_2 = y;
+            
+        }
+        public int total(int x, int y)
+        {
+            return x * 44;
+        }
+    }
+}
