d07c05eaeeccadc74214ee183751e5d358c37562
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-11-19 00:55:22 -0800

    Update Employee.cs

--- Abstract_Class/Abstract_Class/Employee.cs
+++ Abstract_Class/Abstract_Class/Employee.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Abstract_Class
 {
-    class Employee : Person
+    class Employee : Person // inhertited from perosn class 
     {
-        public override void Sayname()
+        public override void Sayname() // overrided inheritance block to allow for method inheritance
         {
             Console.WriteLine(firstname + lastname + "is a team member");
         }
