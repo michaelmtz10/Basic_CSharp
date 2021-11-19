5b2ca767962234897721dec1c71e625396abe656
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-11-19 00:56:36 -0800

    Update Program.cs

--- Abstract_Class/Abstract_Class/Program.cs
+++ Abstract_Class/Abstract_Class/Program.cs
@@ -6,11 +6,11 @@ namespace Abstract_Class
     {
         static void Main(string[] args)
         {
-            Employee employee_1 = new Employee();
+            Employee employee_1 = new Employee(); // created insance of Employee class
 
-            employee_1.firstname = "sample ";
+            employee_1.firstname = "sample "; // initiated method from employee class which inherited from Person class
             employee_1.lastname = "student ";
-            employee_1.Sayname();
+            employee_1.Sayname(); // called method 
         }
     }
 }
