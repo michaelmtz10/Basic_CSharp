686e6fb4b23049c9a723fb6efb394b0a7a4fc6ac
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-20 01:41:17 -0800

    operations_overloading implementation

--- Abstract_Class/Abstract_Class/Employee.cs
+++ Abstract_Class/Abstract_Class/Employee.cs
@@ -8,6 +8,35 @@ namespace Abstract_Class
 {
     class Employee : Person
     {
+        public int employee_Id // created a iD property that'll be assigned to every employee
+        {
+            get
+            {
+                return employee_Id; // grabs property
+            }
+
+            set
+            {
+                employee_Id = value; // sets property to object
+            }
+        }
+        public static bool operator == (Employee employee_1, Employee employee_2) // created operator overloading method
+        {
+            if (((object)employee_1.employee_Id) == null || ((object)employee_2.employee_Id) == null) // checks to see if employees.id duplicate
+                return Object.Equals(employee_1, employee_2); // returns given employee
+
+            return employee_1.Equals(employee_2); // returns bool if emp_1 == emp_2
+        }
+        public static bool operator !=(Employee employee_1, Employee employee_2) 
+        {
+            if (((object)employee_1.employee_Id) == null || ((object)employee_2.employee_Id) == null)
+                return ! Object.Equals(employee_1, employee_2);
+
+            return ! employee_1.Equals(employee_2); // return false if no emp_1 == emp_2
+        }
+
+
+
         public override void Sayname()
         {
             Console.WriteLine(firstname + lastname + "is a team member");
