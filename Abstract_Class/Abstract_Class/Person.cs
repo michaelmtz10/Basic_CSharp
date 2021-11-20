0abfe009ebd0ed07c95fb78f810643f964209eab
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-19 23:53:30 -0800

    interface_class

--- Abstract_Class/Abstract_Class/Person.cs
+++ Abstract_Class/Abstract_Class/Person.cs
@@ -8,17 +8,10 @@ namespace Abstract_Class
 {
     class Person
     {
-<<<<<<< HEAD
         public string firstname; // created attributes
         public string lastname;
 
         public virtual void Sayname() // created method which will add fname and lname into string
-=======
-        public string firstname; //created attributes
-        public string lastname;
-
-        public virtual void Sayname() // created virtual method to allow for inheritance
->>>>>>> 5b2ca767962234897721dec1c71e625396abe656
         {
             Console.WriteLine(firstname + lastname);
         }
