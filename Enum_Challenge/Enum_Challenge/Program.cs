0bae1691da75a39f06f7f9322b6692e6b14a13a6
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-22 11:37:49 -0800

    enum_challenge
    parse through enum options and find corresponding element.

---
+++ Enum_Challenge/Enum_Challenge/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Enum_Challenge
+{
+    class Program
+    {
+        enum DaysoftheWeek // created a enum class
+        {
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
+        }
+
+        static void Main(string[] args)
+        {
+
+            // var for monday
+            DaysoftheWeek Monday = DaysoftheWeek.monday;
+            // var for tuesday
+            DaysoftheWeek Tuesday = DaysoftheWeek.tuesday;
+            // var for wednesday
+            DaysoftheWeek Wednesday = DaysoftheWeek.wednesday;
+            // var for thursday
+            DaysoftheWeek Thursday = DaysoftheWeek.thursday;
+            // var for friday
+            DaysoftheWeek Friday = DaysoftheWeek.friday;
+            // var for saturday
+            DaysoftheWeek Saturday = DaysoftheWeek.saturday;
+            // var for sunday
+            DaysoftheWeek Sunday = DaysoftheWeek.sunday;
+
+            Console.WriteLine("Type in the current day of the week, all in lowercase!"); // ask user to input day of the week
+            string user_choice = Convert.ToString(Console.ReadLine()); // converted user input into string
+                try
+                {
+                    DaysoftheWeek day;
+                    if (Enum.TryParse<DaysoftheWeek>(Convert.ToString(user_choice), true, out day)) // if user input matches any string parsed from DaysotheWeek return to console that it is in fact the correct day.
+                    {
+                        Console.WriteLine("{0}, is the correct day!", day);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is incorrect"); // if user input is not within enum class console.writeline the following...
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+
+                Console.ReadLine();
+            
+        }
+
+    }        
+                  
+}
+
