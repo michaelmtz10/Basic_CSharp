8b97ed775d8054fb81ae1642214411063a504897
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-23 12:13:16 -0800

    lambda
    in_progress

---
+++ Lambda_Assignment/Lambda_Assignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Assignment
+{
+    class Employee
+    {
+        public string first_name;
+
+        public string last_name;
+
+        public int employee_Id // created a iD property that'll be assigned to every employee
+        { get; set; }
+
+        public Employee(string fname, string lname, int id)
+        {
+            first_name = fname;
+            last_name = lname;
+            employee_Id = id;
+        }
+
+    }
+}
---
+++ Lambda_Assignment/Lambda_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambda_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employee_data = new List<Employee>();
+            employee_data.Add(new Employee("michael", "desantis", 1));
+            employee_data.Add(new Employee("john", "smith", 2));
+            employee_data.Add(new Employee("joe", "thomas", 3));
+            employee_data.Add(new Employee("kyle", "kyzer", 4));
+            employee_data.Add(new Employee("joe", "smith", 5));
+            employee_data.Add(new Employee("rob", "desantis", 6));
+            employee_data.Add(new Employee("don", "checho", 7));
+            employee_data.Add(new Employee("larry", "oConnor", 8));
+            employee_data.Add(new Employee("michael", "asumgua", 9));
+            employee_data.Add(new Employee("garry", "lazereth", 10));
+
+            List<string> num_of_joes = employee_data.FindAll()
+            foreach (var joe in num_of_joes)
+            {
+                if (employee_data.FindAll("joe"));
+            }
+        }
+    }
+}
