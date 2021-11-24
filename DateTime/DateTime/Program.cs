2425f492d6b5303d63cff92b6232d51b3770f5b3
michaelmtz10 <m.mtz1088@gmail.com>
2021-11-24 01:47:16 -0800

    datetime app
    adds time to day on user input

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now; // assign current time to variable
+            Console.WriteLine("This is the time right now: {0}", now + "\n\n"); // display current time to console
+            Console.WriteLine("Type in a number to add to the current time.");
+            int response = Convert.ToInt32(Console.ReadLine());
+
+            DateTime latertime = DateTime.Now.AddHours(response); // created object from datetime class, with method add hours, hours added will be the int typed by user
+            Console.WriteLine("This will be the time: {0}, once added by your number", latertime); // writing the operated time to console
+
+
+        }
+        
+
+    }
+}
