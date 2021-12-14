ad89a3c6459e4c90f84e6be7a8593b68ceb5f325
michaelmtz10 <m.mtz1088@gmail.com>
2021-12-13 20:17:17 -0800

    Car_Insurance
    MVC

---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mmtz1\source\repos\CarInsurance\CarInsurance\App_Data\Insurance.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Admin()
+        {
+            string queryString = "Select * From Table";
+
+            List<Table> tables = new List<Table>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Table table = new Table();
+
+                    table.Id = Convert.ToInt32(reader["Id"]);
+                    table.FirstName = reader["FirstName"].ToString();
+                    table.LastName = reader["LastName"].ToString();
+                    table.EmailAddress = reader["EmailAddress"].ToString();
+                    table.DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]);
+                    table.CarYear = Convert.ToInt32(reader["CarYear"]);
+                    table.CarMake = reader["CarMake"].ToString();
+                    table.CarModel = reader["CarModel"].ToString();
+                    table.DUI = Convert.ToBoolean(reader["DUI"]);
+                    table.SpeedingTickets = Convert.ToInt32(reader["SpeedingTickets"]);
+                    table.CoverageType = Convert.ToBoolean(reader["CoverageType"]);
+                    table.Quote_ = Convert.ToDecimal(reader["Quote_"]);
+
+                    tables.Add(table);
+                }
+                connection.Close();
+            }
+            return View(tables);
+        }
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mmtz1\source\repos\CarInsurance\CarInsurance\App_Data\Insurance.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote_")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote_")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Get_Quote([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")]Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                int quote = 50;
+
+                var age = DateTime.Now.Year - table.DateOfBirth.Year;
+
+                if(age < 18)
+                {
+                    quote = quote + 100;
+                }
+                else if (age > 25)
+                {
+                    quote = quote + 25;
+                }
+                else if (age > 18 || age < 25)
+                {
+                    quote = quote + 50;
+                }
+                if (table.CarYear < 2000)
+                {
+                    quote = quote + 25;
+                }
+                else if (table.CarYear > 2015)
+                {
+                    quote = quote + 25;
+                }
+                if (table.CarMake == "Porsche")
+                {
+                    quote = quote + 25;
+                }
+                if (table.CarMake == "Porsche" && table.CarModel == "911 Carrera")
+                {
+                    quote = quote + 25;
+                }
+                if (table.SpeedingTickets > 0)
+                {
+                    quote = quote + (10 * table.SpeedingTickets);
+                }
+                if (table.DUI == true)
+                {
+                    quote = quote + (quote / 4);
+                }
+                if (table.CoverageType == true)
+                {
+                    quote = quote + (quote / 2);
+                }
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+    }
+}
