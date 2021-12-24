9becc2fda2818f3825a62c60633802bd2b8c7483
michaelmtz10 <m.mtz1088@gmail.com>
2021-12-23 21:39:50 -0800

    EF_Code_First
    Final challenge using entity framework, as well as incorporating code first method

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (var data = new Context())
+            {
+                var student1 = new Student() { Id = 1, FirstName = "michael", LastName = "martinez" };
+
+                data.Students.Add(student1);
+                data.SaveChanges();
+            }
+        }
+    }
+}
